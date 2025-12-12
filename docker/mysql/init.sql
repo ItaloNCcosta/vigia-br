7a91e84bcf66b6da5d7bc3cdaf89f531558b206e
ItaloNCcosta <italonccosta@gmail.com>
2025-12-12 15:37:45 -0300

    remove: docker setup

--- docker/mysql/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `vigia_database`
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
-CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON `vigia_database`.* TO 'user'@'%';
-FLUSH PRIVILEGES;
